aa7d9a10b9146991b1302956be355bdf8b87adf2
Xavier Kibet <xavierkibet@gmail.com>
2018-08-10 15:26:45 +0300

    Implement full functionality

--- adsgen/views.py
+++ adsgen/views.py
@@ -5,5 +5,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    last=request.GET.get("last") or "0xDEADBEEF"
+    last=request.GET.get("last") or ""
     return render(request,'index.html',locals())
--- smartad/settings.py
+++ smartad/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'adsgen',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
